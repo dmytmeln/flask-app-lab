1f82191c1ef3b1c4932c110a9fc5e2c3d41847b8
Dmytro Melnyk <dimamel28@gmail.com>
2024-10-15 17:32:57 +0300

    Add /homepage route

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/homepage')
+def home():
+    """View for the Home page of your website."""
+    return f"This is your homepage :) "
+
 @app.route('/')
 def main():
     return "Hello, world!"
