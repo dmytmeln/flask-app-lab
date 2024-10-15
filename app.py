b82f2faf2a3811a963ea9d861aa6af16d94469ca
Dmytro Melnyk <dimamel28@gmail.com>
2024-10-15 17:51:21 +0300

    Move method

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return "Hello, world!"
+
 @app.route('/homepage')
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
     return f"<h1>This is your homepage :) - {agent}</h1>"
-
-@app.route('/')
-def main():
-    return "Hello, world!"
 
 @app.route('/hi/<string:name>')
 def greetings(name):
