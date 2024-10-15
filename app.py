04d1478dc6294b679eaa2afdaa90de2ddf8c8c6f
Dmytro Melnyk <dimamel28@gmail.com>
2024-10-15 17:50:52 +0300

    add dynamic route

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/homepage')
 def home():
     """View for the Home page of your website."""
-    return f"This is your homepage :) "
+    agent = request.user_agent
+    return f"<h1>This is your homepage :) - {agent}</h1>"
 
 @app.route('/')
 def main():
     return "Hello, world!"
+
+@app.route('/hi/<string:name>')
+def greetings(name):
+    name = name.upper()
+    age = request.args.get("age", 0, type=int)
+    return f"Welcome {name=} {age=}"
 
 if __name__ == '__main__':
     app.run(debug = True)
