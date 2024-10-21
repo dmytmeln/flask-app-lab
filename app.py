6eb0548d44b4b61f3d9699ce53cc253a2e28e1d5
Dmytro Melnyk <dimamel28@gmail.com>
2024-10-22 01:00:11 +0300

    created resume

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
+
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
@@ -23,6 +24,23 @@ def admin():
     to_url = url_for("greetings", name="administrator", _external=True)
     print(to_url)
     return redirect(to_url)
+
+skills = [
+        "Java, Spring, Spring Boot, Spring MVC",
+        "Spring Data JPA, Hibernate",
+        "RESTful API, Spring Security",
+        "Thymeleaf, JSP",
+        "HTML, CSS, JavaScript",
+        "SQL (MySQL, PostgreSQL), MongoDB",
+        "JUnit, Mockito",
+        "Maven",
+        "Git, GitHub, GitLab"
+    ]
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html", title="Resume", full_name="Дмитро Мельник", age=19, skills=skills)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
